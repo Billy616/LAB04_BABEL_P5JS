08d70ce1fc9fde18d3d33ae3a7ce23681b072a8c
Billy616 <84603043+Billy616@users.noreply.github.com>
2024-08-07 17:58:54 -0600

    Add files via upload

---
+++ output/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// Declarar variables globales para la posición del círculo
+var x;
+var y;
+function setup() {
+  // Crear el lienzo con un tamaño de 800x600 píxeles
+  createCanvas(800, 600);
+  // Inicializar la posición del círculo al centro del lienzo
+  x = width / 2;
+  y = height / 2;
+}
+function draw() {
+  // Establecer el color de fondo a un gris oscuro
+  background(50);
+
+  // Establecer el color del círculo a un tono azul
+  fill(0, 0, 255);
+  noStroke(); // Desactivar el contorno del círculo
+
+  // Dibujar un círculo en la posición del cursor
+  ellipse(mouseX, mouseY, 50, 50);
+}
